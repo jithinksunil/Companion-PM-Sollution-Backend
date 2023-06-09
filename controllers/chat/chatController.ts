181e18b619b3222fe814f6fb8e88df636d5d1633
jithin k sunil <jithinksunil96@gmail.com>
2023-06-09 23:44:27 +0530

    database repository pattern implemented for chat and notification only

--- controllers/chat/chatController.ts
+++ controllers/chat/chatController.ts
@@ -1,11 +1,11 @@
 import { reqType, resType } from "../../types/expressTypes"
-import chatRepository from "../../dataBaserepository/chatRepository"
+import  { findAllConnections, messageSend, messagesAndChatDetails } from "../../dataBaserepository/chatRepository"
 
 const chatController = {
     connnectionList: (req: reqType, res: resType) => {
         const superUserId = req.body.superUserId
         let connections: any[] = []
-        chatRepository.findAllConnections(connections, superUserId).then((connections) => {
+        findAllConnections(connections, superUserId).then((connections) => {
             res.json({ connections })
 
         }).catch(() => {
@@ -15,7 +15,7 @@ const chatController = {
     startChat: (req: reqType, res: resType) => {
         let { senderId, recieverId } = req.body
         if (senderId !== recieverId) {
-            chatRepository.messagesAndChatDetails(senderId, recieverId).then((result) => {
+            messagesAndChatDetails(senderId, recieverId).then((result) => {
                 res.json(result)
             }).catch(() => {
                 res.json({ errorMessage: 'messages cannot be fetchnow due to database issues' })
@@ -26,7 +26,7 @@ const chatController = {
         }
     },
     sendMessage: (req: reqType, res: resType) => {
-        chatRepository.messageSend(req.body).then((messages)=>res.json({ messages })).catch(()=>{
+        messageSend(req.body).then((messages)=>res.json({ messages })).catch(()=>{
             res.json({ message: 'issues faced in data base while sending messages' })
         })
     }
--- dataBaserepository/chatRepository.ts
+++ dataBaserepository/chatRepository.ts
@@ -5,49 +5,47 @@ import superUserCollection from "../models/superUserSchema"
 import conversationCollection from "../models/conversationShema"
 import messageCollection from "../models/messageShema"
 
-const chatHelpers = {
-    findAllConnections: async (connections: any[], superUserId: any) => {
-        try {
-            let projectmanagers = await projectManagerCollection.find({ superUserId: new Types.ObjectId(superUserId) })
-            connections = projectmanagers
-            let siteEngineers = await siteEngineerCollection.find({ superUserId: new Types.ObjectId(superUserId) })
-            connections = [...connections, ...siteEngineers]
-            let superUser = await superUserCollection.findOne({ _id: new Types.ObjectId(superUserId) })
-            return Promise.resolve([...connections, superUser])
-        } catch (error) {
-            return Promise.reject(error)
-        }
-    },
-    messagesAndChatDetails: async (senderId: any, recieverId: any) => {
 
-        try {
-            senderId = new Types.ObjectId(senderId)
-            recieverId = new Types.ObjectId(recieverId)
-            let conversation = await conversationCollection.findOne({ members: { $all: [senderId, recieverId] } })
-            if (!conversation) {
-                await conversationCollection.insertMany([{ members: [senderId, recieverId] }])
-            }
-            conversation = await conversationCollection.findOne({ members: [senderId, recieverId] })
-            if (conversation) {//conversation can possibly null
-                const messages = await messageCollection.find({ conversationId: conversation._id })
-                return Promise.resolve({ messages, recieverId, conversationId: conversation._id })
-            } else {
-                return Promise.resolve({ messages: [] })
-            }
-        } catch (error) {
-            return Promise.reject(error)
-        }
-
-    },
-    messageSend: async (messageObject: {conversationId:any}) => {
-        try {
-            messageObject.conversationId = new Types.ObjectId(messageObject.conversationId)
-            await messageCollection.insertMany([messageObject])
-            const messages = await messageCollection.find({ conversationId: messageObject.conversationId })
-            return Promise.resolve(messages)
-        } catch (error) {
-            return Promise.reject(error)
-        }
+export const findAllConnections = async (connections: any[], superUserId: any) => {
+    try {
+        let projectmanagers = await projectManagerCollection.find({ superUserId: new Types.ObjectId(superUserId) })
+        connections = projectmanagers
+        let siteEngineers = await siteEngineerCollection.find({ superUserId: new Types.ObjectId(superUserId) })
+        connections = [...connections, ...siteEngineers]
+        let superUser = await superUserCollection.findOne({ _id: new Types.ObjectId(superUserId) })
+        return Promise.resolve([...connections, superUser])
+    } catch (error) {
+        return Promise.reject(error)
     }
 }
-export default chatHelpers
+export const messagesAndChatDetails = async (senderId: any, recieverId: any) => {
+
+    try {
+        senderId = new Types.ObjectId(senderId)
+        recieverId = new Types.ObjectId(recieverId)
+        let conversation = await conversationCollection.findOne({ members: { $all: [senderId, recieverId] } })
+        if (!conversation) {
+            await conversationCollection.insertMany([{ members: [senderId, recieverId] }])
+        }
+        conversation = await conversationCollection.findOne({ members: [senderId, recieverId] })
+        if (conversation) {//conversation can possibly null
+            const messages = await messageCollection.find({ conversationId: conversation._id })
+            return Promise.resolve({ messages, recieverId, conversationId: conversation._id })
+        } else {
+            return Promise.resolve({ messages: [] })
+        }
+    } catch (error) {
+        return Promise.reject(error)
+    }
+
+}
+export const messageSend = async (messageObject: { conversationId: any }) => {
+    try {
+        messageObject.conversationId = new Types.ObjectId(messageObject.conversationId)
+        await messageCollection.insertMany([messageObject])
+        const messages = await messageCollection.find({ conversationId: messageObject.conversationId })
+        return Promise.resolve(messages)
+    } catch (error) {
+        return Promise.reject(error)
+    }
+}
