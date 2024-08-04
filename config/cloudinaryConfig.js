a8b4edbfea4cdf877c94f6d60674923d7406e9ed
jithinksunil <jithinksunil96@gmail.com>
2024-08-05 00:56:25 +0530

    updates

--- config/cloudinaryConfig.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var cloudinary_1 = require("cloudinary");
-cloudinary_1["default"].v2.config({ cloud_name: process.env.CLOUDINARY_NAME, api_key: process.env.CLOUDINARY_API_KEY, api_secret: process.env.CLOUDINARY_API_SECRET });
-module.exports = cloudinary_1["default"].v2;
